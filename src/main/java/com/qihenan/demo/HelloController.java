56a34f53ece50733bec01435b8c801d838c4efe7
qihenan <qihn@casewhen.com.cn>
2019-02-27 16:47:41 +0800

    day_test

--- src/main/java/com/qihenan/demo/HelloController.java
+++ src/main/java/com/qihenan/demo/HelloController.java
@@ -1,6 +1,7 @@
 package com.qihenan.demo;
 
 
+import java.util.Date;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ public class HelloController {
 
     @RequestMapping("/helloTwo")
     public String sayHello(){
-        return "helloTwoThreeFour!!!!!!!!!";
+        return new Date().toString();
     }
 
 }
